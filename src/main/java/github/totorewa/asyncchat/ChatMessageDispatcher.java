d9db1f8600e120e2065c0d9d5c678c7e17350869
Totorewa <totorewa@outlook.com>
2022-01-10 17:26:44 +1300

    Shutdown executor and set thread to daemon. Resolves #1

--- src/main/java/github/totorewa/asyncchat/ChatMessageDispatcher.java
+++ src/main/java/github/totorewa/asyncchat/ChatMessageDispatcher.java
@@ -10,14 +10,25 @@ import java.util.concurrent.Executors;
 
 public class ChatMessageDispatcher {
     private final ExecutorService executor;
+    private boolean disposed;
 
     public ChatMessageDispatcher() {
-        executor = Executors.newSingleThreadExecutor();
+        executor = Executors.newSingleThreadExecutor((r) -> {
+            Thread t = Executors.defaultThreadFactory().newThread(r);
+            t.setDaemon(true);
+            return t;
+        });
     }
 
     public void handleMessage(InGameHud hud, MessageType type, Text message, UUID sender) {
+        if (disposed) return;
         // Not fully async because the thread will still block on the blocklist fetch request
         // but at least the thread being blocked isn't the main thread.
         executor.submit(() -> hud.addChatMessage(type, message, sender));
+    }
+
+    public void dispose() {
+        executor.shutdown();
+        disposed = true;
     }
 }
