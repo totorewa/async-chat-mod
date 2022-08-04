380cec9a73762f979f628b7ec03556eae90b29b8
Derek Lee <derek.evilcornrules@gmail.com>
2022-08-04 00:54:42 -0700

    Update to 1.19.1 (#4)
    * Upgrade Gradle wrapper and fabric-loom
    
    * Update to 1.19.1

--- src/main/java/github/totorewa/asyncchat/mixins/ClientPacketListenerMixin.java
+++ src/main/java/github/totorewa/asyncchat/mixins/ClientPacketListenerMixin.java
@@ -2,7 +2,7 @@ package github.totorewa.asyncchat.mixins;
 
 import github.totorewa.asyncchat.AsyncChatMod;
 import net.minecraft.client.multiplayer.ClientPacketListener;
-import net.minecraft.network.chat.ChatSender;
+import net.minecraft.client.multiplayer.chat.ChatListener;
 import net.minecraft.network.chat.ChatType;
 import net.minecraft.network.chat.PlayerChatMessage;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,15 +12,12 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(ClientPacketListener.class)
 public class ClientPacketListenerMixin {
     @Redirect(
-            method = "handlePlayerChat(Lnet/minecraft/network/protocol/game/ClientboundPlayerChatPacket;)V",
+            method = "handlePlayerChat",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/client/multiplayer/ClientPacketListener;handlePlayerChat(Lnet/minecraft/network/chat/ChatType;Lnet/minecraft/network/chat/PlayerChatMessage;Lnet/minecraft/network/chat/ChatSender;)V"
+                    target = "Lnet/minecraft/client/multiplayer/chat/ChatListener;handleChatMessage(Lnet/minecraft/network/chat/PlayerChatMessage;Lnet/minecraft/network/chat/ChatType$Bound;)V"
             ))
-    private void dispatchMessage(ClientPacketListener self, ChatType chatType,
-                                 PlayerChatMessage message, ChatSender sender) {
-        AsyncChatMod.getChatMessageDispatcher().handleMessage(
-                (ChatMessageHandlerInvoker) self, chatType,
-                message, sender);
+    private void dispatchMessage(ChatListener self, PlayerChatMessage playerChatMessage, ChatType.Bound bound) {
+        AsyncChatMod.getChatMessageDispatcher().handleMessage(self, playerChatMessage, bound);
     }
 }
